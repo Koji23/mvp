61372cc7d812eb56da1b4bf2513253d96cc8c697
Jordan Chong <jordanchong23@gmail.com>
2016-07-11 16:58:32 -0700

    Attempt react bootstrap nav (not working)

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,11 +1,13 @@
 var morgan = require('morgan')
 var bodyParser = require('body-parser');
+var cors = require('cors');
+
 
 module.exports = function(app, express) {
+  app.use(cors());
   app.use(morgan('dev'));  // :method :url :status :response-time ms - :res[content-length]
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
-
 
 };
 
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,8 @@
 
 
 module.exports = function(app, express) {
-  app.get('/', function(req, res){
-    // res.redirect('../../client/index.html');
-  });
+  // app.get('/', function(req, res){
+  //   res.redirect('../../client/index.html');
+  // });
 
 };
