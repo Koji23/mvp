53351eb7f61c64fc6a5b8677b72a728304d96d9d
Jordan Chong <jordanchong23@gmail.com>
2016-07-12 14:51:33 -0700

    Enable rendering of rich text retrieved from DB!

--- client/components/Noteentry.js
+++ client/components/Noteentry.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Panel from 'react-bootstrap/lib/Panel';
 import Well from 'react-bootstrap/lib/Well';
+
+import ReactDOM from 'react-dom';
 import {Editor, EditorState, RichUtils, convertFromRaw} from 'draft-js';
 
-var Noteentry = (props) => (
-  <Panel className="comment">
-    <h4 className="username">{props.note.username}</h4>
-    <span className="note">{props.note.note}</span>
-  </Panel>
-);
+class Noteentry extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      editorState: EditorState.createWithContent(convertFromRaw(JSON.parse(this.props.note.note)) )
+    }
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+  render () {
+    return (
+      <Panel className="comment">
+        <h4 className="username">{this.props.note.username}</h4>
+        <Editor 
+          editorState={this.state.editorState} 
+          onChange={this.onChange}
+        />
+      </Panel>
+    );
+  }
+}
+
 
 export default Noteentry;
--- client/index.js
+++ client/index.js
@@ -44544,6 +44544,8 @@
 	  value: true
 	});
 
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 	var _react = __webpack_require__(1);
 
 	var _react2 = _interopRequireDefault(_react);
@@ -44556,26 +44558,58 @@
 
 	var _Well2 = _interopRequireDefault(_Well);
 
+	var _reactDom = __webpack_require__(33);
+
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+
 	var _draftJs = __webpack_require__(259);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	var Noteentry = function Noteentry(props) {
-	  return _react2.default.createElement(
-	    _Panel2.default,
-	    { className: 'comment' },
-	    _react2.default.createElement(
-	      'h4',
-	      { className: 'username' },
-	      props.note.username
-	    ),
-	    _react2.default.createElement(
-	      'span',
-	      { className: 'note' },
-	      props.note.note
-	    )
-	  );
-	};
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Noteentry = function (_React$Component) {
+	  _inherits(Noteentry, _React$Component);
+
+	  function Noteentry(props) {
+	    _classCallCheck(this, Noteentry);
+
+	    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Noteentry).call(this, props));
+
+	    _this.state = {
+	      editorState: _draftJs.EditorState.createWithContent((0, _draftJs.convertFromRaw)(JSON.parse(_this.props.note.note)))
+	    };
+	    _this.onChange = function (editorState) {
+	      return _this.setState({ editorState: editorState });
+	    };
+	    return _this;
+	  }
+
+	  _createClass(Noteentry, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        _Panel2.default,
+	        { className: 'comment' },
+	        _react2.default.createElement(
+	          'h4',
+	          { className: 'username' },
+	          this.props.note.username
+	        ),
+	        _react2.default.createElement(_draftJs.Editor, {
+	          editorState: this.state.editorState,
+	          onChange: this.onChange
+	        })
+	      );
+	    }
+	  }]);
+
+	  return Noteentry;
+	}(_react2.default.Component);
 
 	exports.default = Noteentry;
 
